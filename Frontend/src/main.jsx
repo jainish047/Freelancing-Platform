ad0a23d30726c0526e16a72021636d885bc665c1
jainish <jdp5420950@gmail.com>
2025-01-22 12:19:31 +0530

    React Router setup

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
 import App from './App.jsx'
+import Signin from './Signin.jsx'
+import Login from './Login.jsx'
+import LandingPage from './LandingPage.jsx'
+
+const Router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    children:[
+      {
+        path:"/",
+        element: <LandingPage/>
+      },{
+        path:"/Login",
+        element: <Login/>
+      },{
+        path:"Signin",
+        element:<Signin/>
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={Router}/>
   </StrictMode>,
 )
