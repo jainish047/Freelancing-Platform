9a2342ac0bda5e17b12d4399fbed0dddbf7084b6
Prince7984 <princepatel1128@gmail.com>
2025-01-27 11:38:14 +0530

    added newPage

--- Frontend/src/components/InputField.jsx
+++ Frontend/src/components/InputField.jsx
@@ -1,19 +1,43 @@
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Form,Formik,Field, ErrorMessage } from "formik";
 
 export default function InputField(props) {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col mb-4">
+      
       <label htmlFor={props.name}>
         {props.label}
         {props.required ? <span className="text-red-600">*</span> : null}
       </label>
-      <Field
-        type={props.type || ""}
-        id={props.name}
-        name={props.name}
-        placeholder={props.placeholder}
-        className="border border-blue-500 rounded p-1"
-      />
+
+      {props.type === "checkbox" && props.options ? (
+        <div>
+          {props.options.map((option, index) => (
+            <label key={index} className="flex items-center space-x-2">
+              <Field
+                type="checkbox"
+                id={`${props.name}-${index}`}
+                name={props.name}
+                value={option}
+                className="mr-2"
+              />
+              <span>{option}</span>
+            </label>
+          ))}
+        </div>
+      ) : (
+        <Field
+          type={props.type || "text"}
+          id={props.name}
+          name={props.name}
+          placeholder={props.placeholder}
+          className="border border-blue-500 rounded p-1"
+          rows={props.rows || ""}
+          cols={props.cols || ""}
+          as={props.as || "input"}
+        />
+      )}
+
+
       <ErrorMessage
         name={props.name}
         component="p"
